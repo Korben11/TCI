550f4638e9fdec89e2a046bba67e549aff50ee05
Linh Bui <kobelinh24@gmail.com>
2018-12-08 19:05:11 +0100

    remove scrawler at initialization

--- Web_Crawler/src/main/java/service/Application.java
+++ Web_Crawler/src/main/java/service/Application.java
@@ -6,12 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        try {
             SpringApplication.run(Application.class, args);
-            Controller.run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
 
