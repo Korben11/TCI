39ca401c124501e933b87404cb3b1cd745564505
Linh Bui <kobelinh24@gmail.com>
2018-12-08 18:18:57 +0100

    init BookController

--- Web_Crawler/src/main/java/service/controllers/BookController.java
+++ Web_Crawler/src/main/java/service/controllers/BookController.java
@@ -1,4 +1,20 @@
 package service.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class BookController {
+
+    private static final String path = "/books";
+    private static final String pathDetail = path + "/{id}";
+
+//    @GetMapping(path)
+//    public List getBooks(){
+//        retrieve book list from jsoup
+//    }
 }
