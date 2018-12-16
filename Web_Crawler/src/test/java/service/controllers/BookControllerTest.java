f09e010f94f8a84e60e85bbebc755eede53e9e00
Unknown <linhhoang_o00o@yahoo.com>
2018-12-16 12:19:46 +0100

    prepare bookController test

---
+++ Web_Crawler/src/test/java/service/controllers/BookControllerTest.java
@@ -0,0 +1,24 @@
+package service.controllers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookControllerTest {
+    BookController book;
+
+    /**
+     * check if method getAllBook() returns a correct list of all books found
+     */
+    @Test
+    public void returnAllRetrievedBooks(){
+        //Arrange
+        book = new BookController();
+        //Act
+        assertEquals();
+        //Assert
+
+    }
+
+}
