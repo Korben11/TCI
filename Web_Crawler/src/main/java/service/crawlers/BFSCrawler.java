f9ac2de7da827e9010da2f6244df56042a3b39ff
Juraj Korbacka <jkorbacka@gmail.com>
2018-12-16 22:20:21 +0100

    prepare tests for crawler

--- Web_Crawler/src/main/java/service/crawlers/BFSCrawler.java
+++ Web_Crawler/src/main/java/service/crawlers/BFSCrawler.java
@@ -1,28 +1,36 @@
 package service.crawlers;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-import java.util.HashSet;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 public class BFSCrawler implements Crawler {
-    private String startingUrl;
-    private Set<String> visitedPages;
+    private URL startingUrl;
+    private List<String> visitedPages;
+//    private Set<String> visitedPages;
     private List<String> currentPages;
     private List<String> nextPages;
     private int maxDepth = 0;
 
-    public BFSCrawler(String startingUrl) {
-        this.startingUrl = startingUrl;
-        this.visitedPages = new HashSet<>();
+    public BFSCrawler(String startingUrl) throws MalformedURLException {
+        this.startingUrl = new URL(startingUrl);
+        this.visitedPages = new LinkedList<>();
         this.currentPages = new LinkedList<>();
         this.nextPages = new LinkedList<>();
     }
 
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+
     @Override
-    public Document next() {
+    public Document next() throws NextPageDoesNotExistsException {
         return null;
     }
 
@@ -31,7 +39,22 @@ public class BFSCrawler implements Crawler {
         return false;
     }
 
-    private void addVisitedPage(String pageUrl) {
+    @Override
+    public boolean matchingDomain(String url) {
+        return false;
+    }
+
+    private int getVisitedPagesCount() {
+        return this.visitedPages.size();
+    }
+
+    /**
+     *
+     * adds new links to unique stack of pages to be crawled
+     *
+     * @param pageUrls list of url links to add
+     */
+    public void addPagesToVisit(LinkedList<String> pageUrls) {
 
     }
 
